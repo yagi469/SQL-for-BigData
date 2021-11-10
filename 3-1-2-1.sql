3afaf451a5bb70c6e06f7229a1721a27418a025c
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-11-10 11:34:57 +0900

    Update 3-1-2-1.sql
    3行目の -- 直後に半角スペースを挿入

--- 3-1-2-1.sql
+++ 3-1-2-1.sql
@@ -1,6 +1,6 @@
 SELECT
     stamp
-  --referrerのホスト各部分を抽出する
+  -- referrerのホスト各部分を抽出する
   -- ■ PostgreSQLの場合、substring関数と正規表現を用いる
   , substring(referrer from 'https?://([^/]*)') AS referrer_host
 FROM access_log
