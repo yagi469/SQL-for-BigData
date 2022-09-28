73b45d941c821b3b6522c70792ae636b1adecaa7
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 17:51:57 +0900

    Create 3-2-5-3.sql

---
+++ 3-2-5-3.sql
@@ -0,0 +1,11 @@
+SELECT
+    user_id
+    
+  -- ■ PostgreSQLの場合、age関数とEXTRACT関数を用いて年単位の年齢を計算できる
+  , CURRENT_DATE AS today
+  , register_stamp::date AS register_date
+  , birth_date::date AS birth_date
+  , EXTRACT(YEAR FROM age(birth_date::date)) AS current_age
+  , EXTRACT(YEAR FROM age(register_stamp::date, birth_date::date)) AS register_age
+FROM mst_users_with_birthday
+;
