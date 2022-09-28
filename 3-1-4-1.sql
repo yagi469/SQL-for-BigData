1ea9038fce3a5173e55612a01f4b3a2866154c06
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 12:13:46 +0900

    Create 3-1-4-1.sql

---
+++ 3-1-4-1.sql
@@ -0,0 +1,10 @@
+SELECT
+  -- ■　PostgreSQL, Hive, BigQueryの場合は、
+  -- CURRENT_DATE定数やCURRENT_TIMESTAMP定数が利用できる
+    CURRENT_DATE AS dt
+  , CURRENT_TIMESTAMP AS stamp
+  
+  -- ■ PostgreSQLの場合、CURRENT_TIMESTAMPはタイムゾーン付きのタイムスタンプ型になる
+  -- タイムゾーンなしのタイムスタンプを取得したい場合は、LOCALTIMESTAMPを用いる
+  -- , LOCALTIMESTAMP AS stamp
+;
