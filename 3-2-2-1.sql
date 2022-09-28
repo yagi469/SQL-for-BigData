35dce0992213117634ae7a36f2176a7f06ed8d81
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 15:31:06 +0900

    Create 3-2-2-1.sql

---
+++ 3-2-2-1.sql
@@ -0,0 +1,19 @@
+SELECT
+    year
+  , q1
+  , q2
+    -- Q1とQ2の売上変化を評価する
+ , CASE
+    WHEN q1 < q2 THEN '+'
+    WHEN q1 = q2 THEN ''
+    ELSE '-'
+  END AS judge_q1_q2
+    -- Q1とQ2の売上額の差を計算する
+  , q2-q1 AS diff_q2_q1
+    -- Q1とQ2の売上変化を1, 0, -1で表現する
+  , SIGN(q2 - q1) AS sign_q2_q1
+FROM
+  quarterly_sales
+ORDER BY
+  year
+;
