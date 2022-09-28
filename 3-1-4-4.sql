1c0da65311dba27a570a8761eae3c3c586209814
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 12:55:15 +0900

    Create 3-1-4-4.sql

---
+++ 3-1-4-4.sql
@@ -0,0 +1,20 @@
+SELECT
+    stamp
+  -- ■ PostgreSQL, Hive, Redshift, SparkSQLでは、substring関数が利用できる
+  , substring(stamp,  1, 4) AS year
+  , substring(stamp,  6, 2) AS month
+  , substring(stamp,  9. 2) AS day
+  , substring(stamp, 12, 2) AS hour
+  -- 年と月を同時に抽出する
+  , substring(stamp,  1, 7) AS year_month
+  
+  -- ■ PostgreSQL, Hive, Redshift, SparkSQLでは、substr関数が利用できる
+  , substr(stamp,  1, 4) AS year
+  , substr(stamp,  6, 2) AS month
+  , substr(stamp,  9. 2) AS day
+  , substr(stamp, 12, 2) AS hour
+  , substr(stamp,  1, 7) AS year_month
+FROM
+  -- ■ PostgreSQL, Redshiftの場合、文字列型としてtextが利用できる
+  (SELECT CAST('2016-01-30 12:00:00' AS text) AS stamp) AS t
+;
