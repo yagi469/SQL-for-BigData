79888036117f18254e154ebb3a2a1d06d5217ac3
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 12:33:03 +0900

    Create 3-1-4-3.sql

---
+++ 3-1-4-3.sql
@@ -0,0 +1,11 @@
+SELECT
+    stamp
+  -- ■ PostgreSQL, Redshift, BigQueryの場合、EXTRACT関数が利用できる
+  , EXTRACT(YEAR  FROM stamp) AS year
+  , EXTRACT(MONTH FROM stamp) AS month
+  , EXTRACT(DAY   FROM stamp) AS day
+  , EXTRACT(HOUR  FROM stamp) AS hour
+  
+FROM
+  (SELECT CAST('2016-01-30 12:00:00' AS timestamp) AS stamp) AS t
+;
