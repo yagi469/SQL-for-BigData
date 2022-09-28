e21cdeab7d1c83f0c4f35a509652c842bfbb6ebe
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 15:50:39 +0900

    Create 3-2-2-5.sql

---
+++ 3-2-2-5.sql
@@ -0,0 +1,11 @@
+SELECT
+    year
+  , (COALESCE(q1, 0) + COALESCE(q2, 0) + COALESCE(q3, 0) + COALESCE(q4, 0))
+    / (SIGN(COALESCE(q1 ,0)) + SIGN(COALESCE(q2, 0))
+      + SIGN(COALESCE(q3, 0) + COALESCE(q4, 0)))
+    AS average
+FROM
+  quaterly_sales
+ORDER BY
+  year
+;
