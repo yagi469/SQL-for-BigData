c3dd9412c3b24035c62728bfd2d1732659b29197
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-28 12:56:47 +0900

    Update 3-1-4-4.sql

--- 3-1-4-4.sql
+++ 3-1-4-4.sql
@@ -3,7 +3,7 @@ SELECT
   -- ■ PostgreSQL, Hive, Redshift, SparkSQLでは、substring関数が利用できる
   , substring(stamp,  1, 4) AS year
   , substring(stamp,  6, 2) AS month
-  , substring(stamp,  9. 2) AS day
+  , substring(stamp,  9, 2) AS day
   , substring(stamp, 12, 2) AS hour
   -- 年と月を同時に抽出する
   , substring(stamp,  1, 7) AS year_month
