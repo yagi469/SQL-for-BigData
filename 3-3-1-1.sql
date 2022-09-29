c2550224a3c845f609d6b912696af3ad31426791
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-29 16:51:35 +0900

    Create 3-3-1-1.sql

---
+++ 3-3-1-1.sql
@@ -0,0 +1,11 @@
+SELECT
+    COUNT(*) AS total_count
+  , COUNT(DISTINCT user_id) AS user_count
+  , COUNT(DISTINCT product_id) AS product_count
+  , SUM(score) AS sum
+  , AVG(score) AS avg
+  , MAX(score) AS max
+  , MIN(score) AS min
+FROM
+  review
+;
