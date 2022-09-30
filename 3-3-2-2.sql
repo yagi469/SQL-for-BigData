d4f9a4aa8d57a273748624ad9caf60141a916f8d
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-30 15:38:20 +0900

    Create 3-3-2-2.sql

---
+++ 3-3-2-2.sql
@@ -0,0 +1,33 @@
+SELECT
+    product_id
+  , score
+    
+    -- スコア順に一意なランキングを付与する
+  , ROW_NUMBER() OVER(ORDER BY score DESC) AS row
+  
+    -- ランキング上位からの累計スコア合計を計算する
+  , SUM(score)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
+    AS cum_score
+    
+    -- いまの行と前後1行ずつの、合計3行の平均スコアを計算する
+  , AVG(score) 
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING)
+    AS local_avg
+    
+    -- ランキング最上位の商品IDを取得する
+  , FIRST_VALUE(product_id)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+    AS first_value
+    
+    -- ランキング最下位の商品IDを取得する
+  , LAST_VALUE(product_id)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+    AS last_value
+FROM popular_products
+ORDER BY row
+;
