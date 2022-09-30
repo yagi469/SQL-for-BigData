afd3b3ea05af8331e480e24373b70c50d6024805
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-09-30 15:52:50 +0900

    Create 3-3-2-3.sql

---
+++ 3-3-2-3.sql
@@ -0,0 +1,30 @@
+SELECT
+    product_id
+    
+    -- スコア順に一意なランキングを付与する
+  , ROW_NUMBER() OVER(ORDER BY score DESC) AS row
+  
+    -- ランキングの最初から最後までの範囲を対象に商品IDを集約
+    -- ■ PostgreSQLの場合はarray_agg, Hive, SparkSQLの場合はcollect_listを使用する
+  , array_agg(product_id)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+    AS whole_agg
+    
+    -- ランキングの最初から現在の行までの範囲を対象に商品IDを集約
+    -- ■ PostgreSQLの場合はarray_agg, Hive, SparkSQLの場合はcollect_listを使用する
+  , array_agg(product_id)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
+    AS cum_agg
+    
+    -- ランキングの1つ前から一つ後までの範囲を対象に商品IDを集約
+    -- ■ PostgreSQLの場合はarray_agg, Hive, SparkSQLの場合はcollect_listを使用する
+  , array_agg(product_id)
+      OVER(ORDER BY score DESC
+        ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING)
+    AS local_agg
+FROM popular_products
+WHERE category = 'action'
+ORDER BY row
+;
