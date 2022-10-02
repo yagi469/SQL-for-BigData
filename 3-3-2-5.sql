3b820ce84c3d3aedbab39666d43679377ec3c0d6
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2022-10-02 10:11:11 +0900

    Create 3-3-2-5.sql

---
+++ 3-3-2-5.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM
+  -- サブクエリ内でランキングを計算
+  ( SELECT
+        category
+      , product_id
+      , score
+        -- カテゴリごとに、スコア順に一意なランキングを付与する
+      , ROW_NUMBER()
+          OVER(PARTITION BY category ORDER BY score DESC)
+        AS rank
+    FROM popular_products
+  ) AS popular_products_with_rank
+-- 外側のクエリでランクを絞り込む
+WHERE rank <= 2
+ORDER BY category, rank
+;
